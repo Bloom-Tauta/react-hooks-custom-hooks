0a3a4f67a6413f613697dc50d6eba006143bce14
Bloom <bloomtauta@gmail.com>
2023-08-19 12:57:41 +0300

    done

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
-
+  const { data: posts, isLoaded } = useQuery("http://localhost:4000/posts");
   useEffect(() => {
     setIsLoaded(false);
     fetch("http://localhost:4000/posts")
@@ -21,6 +21,8 @@ function HomePage() {
   useEffect(() => {
     document.title = "Underreacted | Home";
   }, []);
+
+  useDocumentTitle("Underreacted | Home");
 
   return (
     <>
